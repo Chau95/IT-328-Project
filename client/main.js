47ce94a3054bd61061c79d6c883960389efc9266
Chau Duong <chau.duong1995@gmail.com>
2016-05-02 00:01:50 -0700

    Add some functionalities

--- client/main.js
+++ client/main.js
@@ -1,19 +1,49 @@
 import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor'
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-/*
-Template.addTask.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Session.set('TaskCollection', ' ');
+
+Template.Table.events({
+  'click tr': function(e){
+        e.preventDefault();
+        $('#addTask').modal('show');
+    }
 });
 
 Template.addTask.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+  'click #save': function(e) {
+    e.preventDefault();
+    
+    var taskname = {
+      name: $('#taskname').val()
+    }
+    
+    var hashtags = {
+      name: $('#hashtags').val()
+    }
+
+    Meteor.call('addNewTask', taskname, function(error, result) {
+      if (error) {
+        alert(error);
+      }
+    });
+
+    $('#addTask').modal('hide');
+  }
 });
-*/
+
+Meteor.methods({
+  addNewTask: function(taskname) {
+    check(taskname.name, String);
+
+    _.extend(taskname, {"rank": 0});
+    TaskCollection.insert(taskname);
+  }
+});
+
+
+
+
