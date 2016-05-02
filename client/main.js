45b981adcb15bed6ae4c4fb4521e6863cf004151
Chau Duong <chau.duong1995@gmail.com>
2016-05-01 21:53:14 -0700

    Front end
    v1

---
+++ client/main.js
@@ -0,0 +1,19 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+/*
+Template.addTask.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.addTask.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
+*/
